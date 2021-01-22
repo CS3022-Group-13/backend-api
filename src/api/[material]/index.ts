68196e1de2645acf998407053338ef86830233dd
prathees98 <64325174+prathees98@users.noreply.github.com>
2021-01-22 23:26:44 +0530

    Update index.ts

--- src/api/[material]/index.ts
+++ src/api/[material]/index.ts
@@ -5,6 +5,14 @@ import deleteMaterial from "./delete";
 import viewMaterial from "./view";
 import updateMaterial from "./update"
 
+
+
+import registerStock from "./registerstock";
+import deleteStock from "./deletestock";
+import viewStock from "./viewstock";
+import updateStock from "./updatestock";
+
+
 const rMaterial = Router();
 
 rMaterial.get('/')
@@ -12,5 +20,11 @@ rMaterial.post('/register',registerMaterial)
 rMaterial.delete('/delete/:materialId',deleteMaterial)
 rMaterial.get('/view/:materialId',viewMaterial)
 rMaterial.put('/update/:materialId',updateMaterial)
+
+rMaterial.post('/registerstock',registerStock)
+rMaterial.post('/deletestock/:stockId',deleteStock)
+rMaterial.post('/viewstock/:stockId',viewStock)
+rMaterial.post('/updatestock/:stockId',updateStock)
+
 
 export default rMaterial
