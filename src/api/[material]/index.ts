8e6fe3700eb65501d8fff250e61959afb2dc7ef7
prathees98 <64325174+prathees98@users.noreply.github.com>
2021-01-22 20:33:14 +0530

    Update index.ts

--- src/api/[material]/index.ts
+++ src/api/[material]/index.ts
@@ -1,8 +1,16 @@
 import {Router} from "express";
 
+import registerMaterial from "./register";
+import deleteMaterial from "./delete";
+import viewMaterial from "./view";
+import updateMaterial from "./update"
 
 const rMaterial = Router();
 
 rMaterial.get('/')
+rMaterial.post('/register',registerMaterial)
+rMaterial.delete('/delete/:materialId',deleteMaterial)
+rMaterial.get('/view/:materialId',viewMaterial)
+rMaterial.put('/update/:materialId',updateMaterial)
 
 export default rMaterial
