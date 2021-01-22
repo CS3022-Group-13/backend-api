a406be9bbf3675d60fa6a3b2217ddaa805fd8983
prathees98 <64325174+prathees98@users.noreply.github.com>
2021-01-22 19:53:21 +0530

    Update material.ts

--- src/model/material/material.ts
+++ src/model/material/material.ts
@@ -25,4 +25,31 @@ export class MaterialModel {
         )
         return [error, data[0]]
     }
+    
+     static async createMaterialDataEntry(materialData: Material): Promise<any> {
+        const [error] = await resolver<any>(
+            knex(this.tableName).insert(materialData),
+            {allowUndefined: true});
+        return error;
+    }
+
+    static async deleteBy_materialID(materialID: string): Promise<any> {
+        const error = await resolver<Material[]>(
+            knex(this.tableName).where({materialID}).del(),
+            {
+                singleOnly: true
+            }
+        );
+        return error;
+    }
+
+    static async updateMaterialDataEntry(materialId:string, materialData: Material): Promise<any> {
+        const [error] = await resolver<any>(
+            knex(this.tableName).where(materialId).update(materialData),
+            {allowUndefined: true});
+        return error;
+    }
+
+
 }
+
