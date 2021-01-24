fffe4faff36fe8bc62f9ff5a87b0b59ffdf73fd7
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-01-24 19:43:20 +0530

    add payment permission for customer

--- src/api/[payment]/index.ts
+++ src/api/[payment]/index.ts
@@ -13,6 +13,6 @@ import view from "./view";
 
 rPayment.post("/add", auth.sMan, add);
 rPayment.delete("/remove/:paymentId", auth.sMan, remove);
-rPayment.get("/get-details", auth.sMan, view);
+rPayment.get("/get-details", auth.cus_sMan, view);
 
 export default rPayment;
