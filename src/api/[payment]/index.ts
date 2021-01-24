fb44c4612998ecb280b493a59ceb9cfe8dbc9e2f
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-01-24 10:04:37 +0530

    fix payment endpoints

--- src/api/[payment]/index.ts
+++ src/api/[payment]/index.ts
@@ -12,7 +12,7 @@ import remove from "./delete";
 import view from "./view";
 
 rPayment.post("/add", auth.sMan, add);
-rPayment.delete("/remove/:productId", auth.sMan, remove);
+rPayment.delete("/remove/:paymentId", auth.sMan, remove);
 rPayment.get("/get-details", auth.sMan, view);
 
 export default rPayment;
